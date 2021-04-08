e3105882af10540392baf3a3d747eaf1df7757a8
ashutoshpurohit <purohitji.ashutosh@gmail.com>
2021-04-09 01:54:52 +0530

    Implmented Interfaces and Classes for Domain.Core

---
+++ MicroServicesWithRabbitMQ/MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicroRabbit.Domain.Core.Command;
+using MicroRabbit.Domain.Core.Events;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command.Command;
+
+        void Publish<T>(T _event) where T : Event;
+
+        void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+    }
+}
