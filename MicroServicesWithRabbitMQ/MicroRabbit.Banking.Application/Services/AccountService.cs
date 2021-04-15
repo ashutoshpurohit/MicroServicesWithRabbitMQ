a4bc365717bdb1e20598e960ed1139422b196cf2
ashutoshpurohit <purohitji.ashutosh@gmail.com>
2021-04-15 23:43:22 +0530

    Implementation Pusblish and SendCommand

--- MicroServicesWithRabbitMQ/MicroRabbit.Banking.Application/Services/AccountService.cs
+++ MicroServicesWithRabbitMQ/MicroRabbit.Banking.Application/Services/AccountService.cs
@@ -1,6 +1,9 @@
 ﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Banking.Domain.Models;
+using MicroRabbit.Domain.Core.Bus;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,16 +14,29 @@ namespace MicroRabbit.Banking.Application.Services
 {
     public class AccountService : IAccountService
     {
-        public readonly IAccountRepository _accountRepository;
+        private readonly IAccountRepository _accountRepository;
+        private readonly IEventBus _bus;
 
-        public AccountService(IAccountRepository accountRepository)
+        public AccountService(IAccountRepository accountRepository, IEventBus bus)
         {
             _accountRepository = accountRepository;
+            _bus = bus;
         }
 
         public IEnumerable<Account> GetAccounts()
         {
             return _accountRepository.GetAccounts();
+        }
+
+        public void Transfer(AccountTransfer accountTransfer)
+        {
+            var createTransferCommand = new CreateTransferCommand(
+                from:accountTransfer.FromAccount,
+                to: accountTransfer.ToAccount,
+                amount: accountTransfer.TransferAmount
+                );
+
+            _bus.SendCommand(createTransferCommand);
         }
     }
 }
